53ac596dcd5353fe917eb92a1b093ba1948eb528
merlos <merlos@users.noreply.github.com>
2020-03-22 08:57:35 -0400

    Updated EasyAuthContext

--- src/components/EasyAuthContext.js
+++ src/components/EasyAuthContext.js
@@ -1,7 +1,6 @@
 import React from 'react'
 /**
-** It creates the authenctication context for entire application. 
-** Using this context app can access username, accesstoken, appUrl and graphUrl
+* It creates the authentication context for the entire application. 
 */
 const EasyAuthContext = React.createContext()
 export default EasyAuthContext
--- src/components/EasyAuthContext.md
+++
@@ -1,8 +0,0 @@
-## Example
-
-```js static
-
-import React from 'react'
-import {EasyAuthContext} from '@unicef/react-easyauth'
-
-const authContext = React.useContext(EasyAuthContext)
